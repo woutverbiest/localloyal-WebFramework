777f2003cca987f6f07a644ca7c84e36a66773b8
Wout.Verbiest <wout.verbiest@student.howest.be>
2020-03-26 18:40:06 +0100

    add migration create_rewards_table

---
+++ database/migrations/2020_03_26_142901_create_rewards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRewardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rewards', function (Blueprint $table){
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('shop_id')->nullable(false);
+            $table->foreign('shop_id')->references('id')->on('shops');
+            $table->string('rewardName')->nullable(false);
+            $table->integer('points')->nullable(false);
+            $table->mediumText('description')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rewards');
+    }
+}
