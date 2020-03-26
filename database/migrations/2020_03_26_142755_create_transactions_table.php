f577f60579dc89c1524acec6b200c329ac14d25c
Wout.Verbiest <wout.verbiest@student.howest.be>
2020-03-26 18:39:52 +0100

    add migration create_transaction_table

---
+++ database/migrations/2020_03_26_142755_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table){
+            $table->unsignedBigInteger('user_id')->nullable(false);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('shop_id')->nullable(false);
+            $table->foreign('shop_id')->references('id')->on('shops');
+            $table->integer('points')->default(0);
+            $table->boolean('spend_on_reward')->default(0);
+            $table->timestamps();
+
+            $table->primary(['user_id','shop_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
