a13b11ce1150d28d5599c2d91ef1e7c96d30130a
Wout.Verbiest <wout.verbiest@student.howest.be>
2020-03-26 18:38:58 +0100

    add migration create_shops_table

---
+++ database/migrations/2020_03_26_141609_create_shops_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->BigIncrements('id');
+            $table->string('shopname')->nullable(false);
+            $table->boolean('visible')->nullable(false);
+            $table->unsignedBigInteger('shoptype')->nullable(false);
+            $table->foreign('shoptype')->references('id')->on('shoptypes');
+            $table->mediumText('description')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
