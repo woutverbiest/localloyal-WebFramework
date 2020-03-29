63016f0183a4020b89eb1819222c7045f2d0cee9
Wout.Verbiest <wout.verbiest@student.howest.be>
2020-03-29 22:33:18 +0200

    add seerders for user

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,6 +12,28 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        
+        $users = [
+            [
+                'name' => 'Wout Verbiest',
+                'email' => 'wout.verbiest@student.howest.be',
+                'password' => bcrypt(12345)
+            ],
+            [
+                'name' => 'John Doe',
+                'email' => 'john.doe@mail.com',
+                'password' => bcrypt('johndoe')
+            ],
+            [
+                'name' => 'Jane Doe',
+                'email' => 'jane.doe@mail.com',
+                'password' => bcrypt('janedoe')
+            ]
+        ];
+
+        //TODO PASSWORDS ARE CLEAR IN DATABASE
+
+        foreach($users as $user){
+            User::create($user);
+        }
     }
 }
