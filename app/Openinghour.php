6c57ca94b18b5905711db78fdf0b2c5b7c0f7f8c
Wout.Verbiest <wout.verbiest@student.howest.be>
2020-03-30 16:46:49 +0200

    add fillable and hidden to openinghour model

--- app/Openinghour.php
+++ app/Openinghour.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Openinghour extends Model
 {
-    //
+    protected $fillable = [
+        'shop_id','day_id','from','till','brake_start','brake_end', 'closed'
+    ];
+    protected $hidden =[
+        'created_at','updated_at'
+    ];
 }
