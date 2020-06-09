c6b8cce6c7f26b0cd54fea8cbd6a116780e16a07
Wout.Verbiest <wout.verbiest@student.howest.be>
2020-06-09 16:25:38 +0200

    readme

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,13 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserTableSeeder::class);
+        //$this->call(UserTableSeeder::class);
         $this->call(DaysTableSeeder::class);
         $this->call(ShopTypeTableSeeder::class);
-        $this->call(ShopTableSeeder::class);
-        $this->call(OpeninghoursTableSeeder::class);
-        $this->call(RewardsTableSeeder::class);
-        $this->call(TransactionTableSeeder::class);
-        $this->call(UserShopTableSeeder::class);
+        //$this->call(ShopTableSeeder::class);
+        //$this->call(OpeninghoursTableSeeder::class);
+        //$this->call(RewardsTableSeeder::class);
+        //$this->call(TransactionTableSeeder::class);
+        //$this->call(UserShopTableSeeder::class);
     }
 }
